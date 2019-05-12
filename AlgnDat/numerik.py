c36397d43151b99743d96f774c40d5459b9dbdbd
user <tim.buchholz98@gmx.de>
2019-05-12 12:07:43 +0200

    num lr ???

--- AlgnDat/numerik.py
+++ AlgnDat/numerik.py
@@ -13,25 +13,20 @@ def a_skalar(A, u, w):
 def a_norm(x, A):
     return np.sqrt(a_skalar(A, x, x))
 
-
-def LU_einzeln(A):  # Todo: bug
+def LUdecomp(A):
     '''
-    inplace
-    :param A: np.matrix()  nxn
-    :return: LR np.matrix() saved in one matrix  nxn
+    basic funcion of LUDecomp in single notation
+    :param A: np.matrix() nxn
+    :return:  LR np.matrix() can be splitted to L and U with funcs below
     '''
     n = A[0].size
-
-    for k in range(n):
-        if np.abs(A[k, k]) < eps:
-            raise ZeroDivisionError("LR - Zerlegung exitiert nicht")
-
-        for i in range(k + 1, n - 1):
-            A[i, k] = A[i, k] / A[k, k]
-            for j in range(k + 1, n - 1):
-                A[i, j] = A[i, j] - A[i, k] * A[k, j]
+    for k in range(0,n-1):
+        for i in range(k+1,n):
+            if A[k,k] != 0.0:
+                lam = A[i,k]/A[k,k]
+            A[i,k+1:n]=A[i,k+1:n]-lam*A[k,k+1:n]
+            A[i,k] = lam
     return A
-
 
 def LUP_solve(A, b):
     '''
@@ -43,26 +38,25 @@ def LUP_solve(A, b):
     p = np.array(range(0, n)).reshape(n, 1)
 
     for i in range(0, n - 1):
-        r = np.amax(np.abs(A[i:n - 1, n - 1]))  # max value ls(i:n)
-        m = np.where(np.abs(A[i:n - 1, n - 1]) == r)[0][0] + i  # max index ls(i:n)
-        if np.abs(A[m, n - 1]) < eps:
-            raise ZeroDivisionError("Matrix fast singulär")
-        if np.isclose(m, n - 1):  # Todo matlab is ~=
-            A[[n - 1, m], :] = A[[m, n - 1], :]
-            p[[n - 1, m]] = p[[m, n - 1]]
-        A[i + 1:n - 1, i] = A[i + 1:n - 1, i] * 1.0 / A[i, i]
-        A[i + 1:n - 1, i + 1:n - 1] = A[i + 1:n - 1, i + 1:n - 1] - A[i + 1:n - 1, i] * A[i, i + 1:n - 1]
+        #r = np.amax(np.abs(A[i:n - 1, i]))  # max value ls(i:n)
+        #m = np.where(np.abs(A[i:n - 1, i]) == r)[0][0] + i  # max index ls(i:n)
+        #if np.abs(A[m, n - 1]) < eps:
+        #    raise ZeroDivisionError("Matrix fast singulär")
+        #if m != i:
+         #   A[[i, m], :] = A[[m, i], :]
+        #    p[[i, m]] = p[[m, i]]
+        A[i + 1:, i] = A[i + 1:, i] * 1.0 / A[i, i]
+        A[i + 1:, i + 1:] = A[i + 1:, i + 1:] - A[i + 1:, i] * A[i, i + 1:]
+    print(A)
+    x = b.copy()
+    #for i in range(n):
+        #x[i, 0] = b[p[i, 0], 0]
+    for i in range(1, n):
 
-    x = np.array(np.ones(n)).reshape(n, 1)
-    for i in range(n):
-        x[i,0] = b[p[i,0],0]
+        x[i] = x[i] - A[i, :i] * x[:i]
 
-    for i in range(1,n):
-        x[i,0] = x[i,0] - A[i,0:i+1]* x[0:i+1,0].reshape(i+1,1)
-
-    for i in range(n-1,-1,-1):
-        pass #todo cont
-
+    for i in range(n - 1, -1, -1):
+        x[i] = (x[i] - A[i, i + 1:n + 1] * x[i + 1:n + 1]) / A[i, i]
 
 
 
@@ -93,4 +87,21 @@ print(LU_einzeln(A))
 t2 = time.time() - t0
 print("T1 : " + str(t1) + "  T2 : " + str(t2))
 '''
-LUP_solve(A, b )
+LUP_solve(A, b)
+A = np.matrix([[1, 4, 3, 12],
+               [3, 1, 2, 9],
+               [1, 2, 1, 10],
+               [1, 1, 5, 1]])
+b = np.array([[1],
+              [2],
+              [3],
+              [4]])
+P, L, U = scipy.linalg.lu(A)
+#print(L)
+#print(np.linalg.solve(A, b))
+
+A = np.matrix([[1, 4, 3, 12],
+               [3, 1, 2, 9],
+               [1, 2, 1, 10],
+               [1, 1, 5, 1]])
+print(LUdecomp(A))
